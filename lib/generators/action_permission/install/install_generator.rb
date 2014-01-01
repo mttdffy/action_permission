91b6b79accd3264eae10333ec375c65e8b151f4e
Matt Duffy <mttdffy@gmail.com>
2014-01-01 23:09:33 +0000

    adding controlelr code injection to install generator

--- lib/generators/action_permission/install/install_generator.rb
+++ lib/generators/action_permission/install/install_generator.rb
@@ -10,6 +10,27 @@ module ActionPermission
       def copy_init_file
         copy_file "init.rb", "app/permissions/application_permission.rb"
       end
+      
+      def add_controller_setup
+        line = "class ApplicationController < ActionController::Base"
+        inject_into_file 'app/controllers/application_controller.rb', after: "#{line}\n" do <<-'RUBY'
+          authorize_with :current_user
+          before_action :check_permission
+          
+          def current_user
+            @current_user ||= session[:user_id] ? User.find(session[:user_id]) : User.new
+          end
+          
+          def check_permission
+            unless authorized?
+              #do something when user does not have permission to access page
+              # Flash[:warn] = "You do not have permission to access this page."
+              # redirect_to root_url
+            end
+          end
+        RUBY
+        end
+      end
     end
 
   end
